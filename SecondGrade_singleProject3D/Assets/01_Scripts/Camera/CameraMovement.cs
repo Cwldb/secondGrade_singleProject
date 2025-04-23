37b4dce1d9f1014290eb538ef09b644fbf3c0540
Cwldb <mozi0810@gmail.com>
2025-04-23 23:57:24 +0900

    add defaultSetting

---
+++ SecondGrade_singleProject3D/Assets/01_Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [field: SerializeField] private PlayerInputSO playerInput;
+    [SerializeField] private float moveSpeed = 5;
+
+    private void Update()
+    {
+        Vector3 moveDir = new Vector3(playerInput.MovementKey.x, 0, playerInput.MovementKey.y);
+        transform.position += moveDir * 5 * Time.deltaTime;
+    }
+}
