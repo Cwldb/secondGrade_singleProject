f6a71032bb741323dafd270253439a37ca40037b
Cwldb <mozi0810@gmail.com>
2025-06-01 15:44:53 +0900

    add Ui

---
+++ SecondGrade_singleProject3D/Assets/01_Scripts/UI/LevelupPanel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace _01_Scripts.UI
+{
+    public class LevelupPanel : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
