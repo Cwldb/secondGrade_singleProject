5f84e58c8bafc9ef2ab31b947ae7ee0aeed89e03
Cwldb <mozi0810@gmail.com>
2025-06-07 22:46:14 +0900

    add PlayerInfo

--- SecondGrade_singleProject3D/Assets/01_Scripts/Players/PlayerStat.cs
+++ SecondGrade_singleProject3D/Assets/01_Scripts/Players/PlayerStat.cs
@@ -11,10 +11,61 @@ namespace _01_Scripts.Players
     {
         public StatSO atkPowerStat, critPowerStat, critPerStat, atkSpeedStat;
 
-        public float Damage { get; set; }
-        public float CritPer { get; set; }
-        public float CritPower { get; set; }
-        public float AttackSpeed { get; set; }
+        public Action OnStatValueChanged;
+
+        private float _damage;
+        private float _critPer;
+        private float _critPower;
+        private float _attackSpeed;
+        
+        public float Damage
+        {
+            get => _damage;
+            set
+            {
+                if (_damage != value)
+                {
+                    _damage = value;
+                    OnStatValueChanged?.Invoke();
+                }
+            }
+        }
+        public float CritPer
+        {
+            get => _critPer;
+            set
+            {
+                if (_critPer != value)
+                {
+                    _critPer = value;
+                    OnStatValueChanged?.Invoke();
+                }
+            }
+        }
+        public float CritPower
+        {
+            get => _critPower;
+            set
+            {
+                if (_critPower != value)
+                {
+                    _critPower = value;
+                    OnStatValueChanged?.Invoke();
+                }
+            }
+        }
+        public float AttackSpeed
+        {
+            get => _attackSpeed;
+            set
+            {
+                if (_attackSpeed != value)
+                {
+                    _attackSpeed = value;
+                    OnStatValueChanged?.Invoke();
+                }
+            }
+        }
         
         private Entity _entity;
         private EntityStat _statCompo;
@@ -25,6 +76,11 @@ namespace _01_Scripts.Players
             _entity = entity;
             _statCompo = entity.GetCompo<EntityStat>();
             _healthCompo = entity.GetCompo<EntityHealth>();
+        }
+
+        private void Start()
+        {
+            OnStatValueChanged.Invoke();
         }
 
         public void AfterInitialize()
---
+++ SecondGrade_singleProject3D/Assets/01_Scripts/UI/PlayerInfoPanel.cs
@@ -0,0 +1,31 @@
+﻿using _01_Scripts.Core;
+using _01_Scripts.Players;
+using TMPro;
+using UnityEngine;
+
+namespace _01_Scripts.UI
+{
+    public class PlayerInfoPanel : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text atkText;
+        [SerializeField] private TMP_Text aspdText;
+        [SerializeField] private TMP_Text criText;
+        [SerializeField] private TMP_Text dcriText;
+        
+        private PlayerStat _playerStat;
+
+        private void Start()
+        {
+            _playerStat = GameManager.Instance.PlayerFinder.Target.GetCompo<PlayerStat>();
+            _playerStat.OnStatValueChanged += UpdateStat;
+        }
+
+        private void UpdateStat()
+        {
+            atkText.text = $"{_playerStat.Damage}";
+            aspdText.text = $"{_playerStat.AttackSpeed}";
+            criText.text = $"{_playerStat.CritPer * 100}";
+            dcriText.text = $"{_playerStat.CritPower}";
+        }
+    }
+}
