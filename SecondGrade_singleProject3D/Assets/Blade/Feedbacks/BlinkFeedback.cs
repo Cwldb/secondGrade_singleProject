ed825d702521e59a5a8badf7b008e2370d14061b
Cwldb <mozi0810@gmail.com>
2026-05-06 11:05:56 +0900

    Revert "fix."
    This reverts commit 5bcfe52e4dea548931d58c6bafd3a5da8e481683.

--- SecondGrade_singleProject3D/Assets/Blade/Feedbacks/BlinkFeedback.cs
+++ SecondGrade_singleProject3D/Assets/Blade/Feedbacks/BlinkFeedback.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Assets.Blade.Feedbacks;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class BlinkFeedback : FeedBack
